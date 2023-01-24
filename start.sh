2f21699540cfd1964e1bf08ab80d2a88013a9c74
Aayush <bhaskarsharma64047@gmail.com>
2023-01-24 22:58:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Royaldeep1/DoraemonXRobot1 /DoraemonXRobot1
+  git clone https://github.com/Ayush64047/DoraemonXRobot /DoraemonXRobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DoraemonXRobot1
+  git clone $UPSTREAM_REPO /DoraemonXRobot
 fi
 cd /DoraemonXRobot1
 pip3 install -U -r requirements.txt
