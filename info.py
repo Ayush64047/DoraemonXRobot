d0dc1b635c42387c08db50a80c8c03cb2ea9e1f6
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-10-06 16:03:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/a97005b4dd8c414e9
 NEWGRP = environ.get("NEWGRP", "https://te.legra.ph/file/a97005b4dd8c414e961ba.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1782834874').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1413071149').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001877356372').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -37,7 +37,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Ayush:Ayush@cluster0.efrmiaj.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
