f7271c4de7e3122edbea7d2d104746e2d94f6bb0
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-10-06 17:09:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,9 +37,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Ayush64047:Ayush@cluster1.0rkzbkj.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Z_Harbour')
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
 
 # Channel Button Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/blackest_harbour')
